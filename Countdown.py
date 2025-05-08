9fa5fb042f2c64968e685d8f53e826492e3cd647
Michelleeeeee03 <michellereyes7345@gmail.com>
2025-05-08 11:37:05 -0400

    dropdown

--- Countdown.py
+++ Countdown.py
@@ -3,4 +3,12 @@ import flet as ft
 def main (page: ft.Page):
     page.title = "Countdown App"
 
+    month_dropdown = ft.Dropdown(label="12 AM", 
+        options=[
+            ft.dropdown.Option("12 AM"), 
+            ft.dropdown.Option("01 AM"), 
+            ft.dropdown.Option("02 AM"),
+            ])
+    
+
 ft.app(target=main)

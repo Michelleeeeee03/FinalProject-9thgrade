7e58cb72c9ca224a6e24cac352f4b46949a3053f
esmeralda9th <esmeraldajosecruzh2@gmail.com>
2025-05-15 11:02:05 -0400

    date and time functions

--- Countdown.py
+++ Countdown.py
@@ -11,5 +11,25 @@ def main (page: ft.Page):
     page.title = "Countdown App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    def main (page: ft.Page):
+        page.title = "Countdown App"
+        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    def handle_change(e):
+        page.add(ft.Text(f"Date changed: {e.control.value.strftime('%m/%d/%Y')}"))
+
+    def handle_dismissal(e):
+        page.add(ft.Text(f"DatePicker dismissed"))
+
+    #------------------------------------------------------------------------
+
+    def handle_change(e):
+        page.add(ft.Text(f"TimePicker change: {time_picker.value}"))
+
+    def handle_dismissal(e):
+        page.add(ft.Text(f"TimePicker dismissed: {time_picker.value}"))
+
+    def handle_entry_mode_change(e):
+        page.add(ft.Text(f"TimePicker Entry mode changed to {e.entry_mode}"))
 
 ft.app(target=main)

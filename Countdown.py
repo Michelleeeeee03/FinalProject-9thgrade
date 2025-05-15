e490fbc7946c5bf7f38f7459396f51c33d3c4e23
Michelleeeeee03 <michellereyes7345@gmail.com>
2025-05-15 10:56:04 -0400

    Credits and page alignment

--- Countdown.py
+++ Countdown.py
@@ -1,8 +1,15 @@
+#Credits: 
+# https://flet.dev/docs/controls/datepicker/
+#https://flet.dev/docs/controls/timepicker/
+
 import flet as ft
 import flet_timer as ftt
 import time
+import datetime
 
 def main (page: ft.Page):
     page.title = "Countdown App"
-    
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+
 ft.app(target=main)

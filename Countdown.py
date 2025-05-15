e278db38c376eeea4445f777a106815c5e45d6fa
emelyrod18 <emelyrodriguez1810@gmail.com>
2025-05-15 11:10:46 -0400

    time picker and date picker properties and buttons :)

--- Countdown.py
+++ Countdown.py
@@ -32,4 +32,32 @@ def main (page: ft.Page):
     def handle_entry_mode_change(e):
         page.add(ft.Text(f"TimePicker Entry mode changed to {e.entry_mode}"))
 
+    time_picker = ft.TimePicker(
+            confirm_text="Confirm",
+            error_invalid_text="Time out of range",
+            help_text="Pick your time slot",
+            on_change=handle_change,
+            on_dismiss=handle_dismissal,
+            on_entry_mode_change=handle_entry_mode_change,
+        )
+
+    date_picker = ft.DatePicker(
+        first_date=datetime.datetime(year=2025, month=1, day=1),
+        last_date=datetime.datetime(year=2050, month=1, day=1),
+        on_change=handle_change,
+        on_dismiss=handle_dismissal,
+        )
+
+        
+    page.add(ft.ElevatedButton(
+         "Pick date",
+        icon=ft.Icons.CALENDAR_MONTH,
+        on_click=lambda e: page.open(date_picker),
+        ),
+        ft.ElevatedButton(
+        "Pick time",
+        icon=ft.Icons.TIME_TO_LEAVE,
+        on_click=lambda _: page.open(time_picker),
+        ))
+    
 ft.app(target=main)
